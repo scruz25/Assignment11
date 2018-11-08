23a90c5443cc2a0ef50e7ca07cc0102c091e76f3
Sandra <scruz25@fordham.edu>
2018-11-08 14:22:55 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  $('button').click(function NewTask() {
+  		var inputItem = document.getElementById("AddTask").value;
+  		var i = document.createTextNode(inputItem);
+  		var newItem = document.createElement("li");
+  		newItem.appendChild(i);
+
+      document.getElementById("MyActualRealToDoList").appendChild(newItem);
+  	});
+
+
+  	$('ol').on('click', 'li', function(){
+      	$(this).hide("show");
+      });
+
+});
